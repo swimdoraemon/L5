7bd57c73555580efbb75183b9c4afe2d75d587b0
swimdoraemon <indoaradhya@gmail.com>
2024-09-26 19:21:58 +0900

    attendece

---
+++ Attendce.py
@@ -0,0 +1,14 @@
+medical_cause=input("do you have a medical cause Y or N: ")
+
+
+atten = int(input("enter the attendance of the student:"))
+
+
+
+if medical_cause == 'Y':
+    print ("you are allowed")
+else:
+    if atten>=75:
+        print ("allowed")
+    else:
+        print ("not allowed")
